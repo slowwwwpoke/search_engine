8436c67c2d579ea9cb732caf1959af622fd846db
Hugo Yang <114716697+slowwwwpoke@users.noreply.github.com>
2025-08-23 23:28:18 -0400

    api url update for frontend

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://https://search-engine-gc3w.onrender.com"; 
+const API_URL = "https://search-engine-gc3w.onrender.com"; 
 
 async function search() {
   const query = document.getElementById("query").value.trim();
